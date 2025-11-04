8cf832fcc781beaab660a4b755498ac4b27bd766
Michael Kofman <dnainform@gmail.com>
2025-11-03 21:41:49 -0500

    chore: stop tracking build artifacts (.vercel/, dist/) and sync with origin

--- .vercel/output/static/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-
-// bust 1761541711
-
-// bust 1761542012
-
-// bust 1761543607
